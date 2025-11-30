c929432656b54b16462d6379dab06719681c703f
Bruno Barbosa <br.barbosa.souza1@gmail.com>
2025-11-30 11:29:54 -0300

    feat: Added the model layer

---
+++ src/main/java/dev/bruno/PersonRegistry/model/AdressModel.java
@@ -0,0 +1,90 @@
+package dev.bruno.PersonRegistry.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+@Table(name = "adress")
+@Entity
+public class AdressModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "adress", nullable = false, unique = true)
+    private String adress;
+
+    @Column(name = "number")
+    private short number;
+
+    @Column(name = "neighborhood")
+    private String neighborhood;
+
+    @OneToMany(mappedBy = "adress") // One address for multiple person
+    @JsonIgnore
+    private List<PersonModel> person;
+
+    public AdressModel(){
+
+    }
+
+    public AdressModel(String adress, short number, String neighborhood,  List<PersonModel> person) {
+        this.adress = adress;
+        this.number = number;
+        this.neighborhood = neighborhood;
+        this.person = person;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public short getNumber() {
+        return number;
+    }
+
+    public void setNumber(short number) {
+        this.number = number;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public List<PersonModel> getPerson() {
+        return person;
+    }
+
+    public void setPerson(List<PersonModel> person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "AdressModel{" +
+                "id=" + id +
+                ", adress='" + adress + '\'' +
+                ", number=" + number +
+                ", neighborhood='" + neighborhood + '\'' +
+                ", person=" + person +
+                '}';
+    }
+}
---
+++ src/main/java/dev/bruno/PersonRegistry/model/PersonModel.java
@@ -0,0 +1,69 @@
+package dev.bruno.PersonRegistry.model;
+
+import jakarta.persistence.*;
+
+
+@Table(name = "person")
+@Entity
+public class PersonModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "adress_id")
+    private AdressModel adress;
+
+    public PersonModel() {
+    }
+
+    public PersonModel(String name, String email, AdressModel adress) {
+        this.name = name;
+        this.email = email;
+        this.adress = adress;
+    }
+
+   public Long getId() {
+        return id;
+   }
+
+   public void setId(Long id) {
+        this.id = id;
+   }
+
+   public String getName() {
+        return name;
+   }
+
+   public void setName(String name) {
+        this.name = name;
+   }
+
+   public String getEmail() {
+        return email;
+   }
+
+   public void setEmail(String email) {
+        if (!email.contains("@") || !email.contains(".")) {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+        this.email = email;
+   }
+
+    @Override
+    public String toString() {
+        return "PersonModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", adress=" + adress +
+                '}';
+    }
+}
